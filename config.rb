7e9de4aca3a20d0de74fca8197dd680a69d62b8c
deepakkumarsharma <deeky.sharma@gmail.com>
2014-12-20 07:16:43 +0530

    more slides

--- config.rb
+++ config.rb
@@ -1,11 +1,12 @@
 require 'compass/import-once/activate'
+require 'susy'
 # Require any additional compass plugins here.
 
 # Set this to the root of your project when deployed:
 http_path = "/"
 css_dir = "stylesheets"
 sass_dir = "sass"
-images_dir = "images"
+images_dir = "tasveer"
 javascripts_dir = "javascripts"
 
 # You can select your preferred output style here (can be overridden via the command line):
